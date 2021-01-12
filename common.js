e5106547c40ac5ba8ef8b70c685a64a3dc4e1009
ysh9698 <ysh9698@naver.com>
2021-01-12 22:54:09 +0900

    html 1일차

--- common.js
+++ common.js
@@ -63,7 +63,7 @@ function renderCodepen(wrapperId, url) {
 
 function Editor__init() {
   $('.toast-ui-editor').each(function(index, node) {
-    var initialValue = $(node).prev().html().trim().replace(/<!--REPLACE:script-->/gi, 'script');
+    var initialValue = $(node).prev().html().trim().replace(/t-script/gi, 'script');
 
     var editor = new toastui.Editor({
       el: node,
@@ -74,11 +74,14 @@ function Editor__init() {
     });
   });
 }
-Editor__init();
+
+$(function() {
+  Editor__init();
+});
 
 function EditorViewer__init() {
   $('.toast-ui-viewer').each(function(index, node) {
-    var initialValue = $(node).prev().html().trim().replace(/<!--REPLACE:script-->/gi, 'script');
+    var initialValue = $(node).prev().html().trim().replace(/t-script/gi, 'script');
     var viewer = new toastui.Editor.factory({
       el: node,
       initialValue: initialValue,
@@ -87,6 +90,9 @@ function EditorViewer__init() {
     });
   });
 }
-EditorViewer__init();
+
+$(function() {
+  EditorViewer__init();
+});
 
 /* 토스트 UI 끝 */
