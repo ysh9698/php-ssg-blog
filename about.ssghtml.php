c20918db05a86b36f4e80f744c7d792dd9c73436
ysh9698 <75485765+ysh9698@users.noreply.github.com>
2021-01-27 18:27:12 +0900

    메타태그 수정

--- about.ssghtml.php
+++ about.ssghtml.php
@@ -1,8 +1,8 @@
 <?php
 require_once "data.php";
 
-$pageTitle = "{$siteTitle} - YSH 소개";
-$pageDescription = "YSH에 대한 소개페이지 입니다.";
+$pageTitle = "{$siteTitle} - 주인장 소개";
+$pageDescription = "주인장에 대한 소개페이지 입니다.";
 
 require_once "head.php";
 ?>
--- app.php
+++ app.php
@@ -22,72 +22,51 @@ function getArticleListByTagLink($tag) {
 }
 
 function getFilesByEndsWith($endsWith) {
-
     $fileNames = [];
-
     $it = new RecursiveDirectoryIterator(".");
-
     foreach(new RecursiveIteratorIterator($it) as $file) {
         if ( endsWith($file->getFilename(), $endsWith) ) {
             $fileNames[] = $file->getFilename();
         }
     }
-
     return $fileNames;
 }
-
 function getFiles() {
-
     $fileNames = [];
-
     $it = new RecursiveDirectoryIterator(".");
-
     foreach(new RecursiveIteratorIterator($it) as $file) {
         if ( $file->isDir() ) {
             continue;
         }
-
         if ( strpos($file->getRealPath(), '\.git') !== false ) {
             continue;
         }
-
         if ( strpos($file->getRealPath(), '\docs') !== false ) {
             continue;
         }
-
         $fileNames[] = $file->getRealPath();
     }
-
     return $fileNames;
 }
-
 function getMaxArticleId() {
     static $maxId;
-
     if ( $maxId !== null ) {
         return $maxId;
     }
-
     $keys = array_keys($GLOBALS);
-
     $maxId = 0;
-
     foreach ( $keys as $key ) {
         if ( startsWith($key, "article") ) {
             $no = intval(str_replace("article", "", $key));
-
             if ( $no > $maxId ) {
                 $maxId = $no;
             }
         }
     }
-
     return $maxId;
 }
-
 function &getArticles() {
     global $_allArticles;
-
     return $_allArticles;
 }
 
@@ -164,19 +143,14 @@ function &getArticleById($id) {
 
     return $articles[$id];
 }
-
 function &getArticlesByTag($tag) {
     global $_allArticlesByTag;
-
     if ( isset($_allArticlesByTag[$tag]) == false ) {
         return [];
     }
-
     return $_allArticlesByTag[$tag];
 }
-
 function &getTags() {
     global $_tags;
-
     return $_tags;
 }
