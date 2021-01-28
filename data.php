d2432b32191c8ffa99ef0eda8ca9d09eb490144f
ysh9698 <75485765+ysh9698@users.noreply.github.com>
2021-01-28 14:19:26 +0900

    .

--- data.php
+++ data.php
@@ -15,10 +15,10 @@ $tagInfos = [
         //"pageDescription" => "2021년 특별기획, 자바스크립트 관련 강좌 입니다."
     ],
     "HTML" => [
-        "pageThumbUrl" => "https://github.com/ysh9698/my-img/blob/master/HTML%20Thumbnail%20.jpg?raw=true"
-        "pageDescription" => "2021년 특별기획, HTML 관련 강좌 입니다."
+        "pageThumbUrl" => "https://github.com/ysh9698/my-img/blob/master/HTML%20Thumbnail%20.jpg?raw=true",
+        "pageDescription" => "2021년 특별기획, HTML 관련 강좌 입니다.",
         //"pageThumbUrl" => "html 관련 글 전용 이미지"
-    ]
+    ],
 ];
 // 태그정보 끝
 
